1a1149d06e0871cd6b40f06c08c0cf3c7af425ff
MizoTake <mizotake123@gmail.com>
2020-02-02 14:13:49 +0900

    UnityProject

---
+++ UnityProject/Assets/Scripts/SampleText.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+using TMPro;
+using UnityEngine;
+
+namespace ApplePlatformNativePlugin {
+
+    public class SampleText : MonoBehaviour
+    {
+
+        [SerializeField] private TextMeshProUGUI centerText;
+        
+#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+        private const string LibraryName = "XcodeProject";
+#elif UNITY_IOS && !UNITY_EDITOR
+        private const string LibraryName = "__Internal";
+#endif
+        
+        
+        [DllImport (LibraryName)]
+        private static extern string CallNativeString();
+        
+        void Start()
+        {
+            centerText.text = CallNativeString();
+        }
+    }
+    
+}
